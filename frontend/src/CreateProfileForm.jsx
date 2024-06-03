99c9edf219c53bed5e150328e1b0b20441ab4637
TigerPanda77 <152322803+TigerPanda77@users.noreply.github.com>
2024-06-03 09:01:37 -0400

    newStuff

--- frontend/src/CreateProfileForm.jsx
+++ frontend/src/CreateProfileForm.jsx
@@ -7,8 +7,8 @@ import './index.css';
 const CreateProfileForm = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [newUsername, setNewUsername] = useState('');
+  const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
@@ -31,7 +31,7 @@ const CreateProfileForm = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, firstName, lastName, password }),
+        body: JSON.stringify({ newUsername, firstName, lastName, newPassword }),
       });
 
       const data = await response.json();
@@ -78,22 +78,22 @@ const CreateProfileForm = () => {
           />
         </div>
         <div>
-          <label htmlFor="username">Username:</label>
+          <label htmlFor="newUsername">Username:</label>
           <input
             type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            id="newUsername"
+            value={newUsername}
+            onChange={(e) => setNewUsername(e.target.value)}
             required
           />
         </div>
         <div>
-          <label htmlFor="password">Password:</label>
+          <label htmlFor="anewPssword">Password:</label>
           <input
             type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            id="newPassword"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
             required
           />
         </div>
